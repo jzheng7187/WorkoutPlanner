c051e66fb472befe9b17dec251f601b2136a8c57
Student 6 <Student 6@10.8.33.152>
2017-02-02 12:20:13 -0500

    Created a Alex file

---
+++ WorkoutPlanner/src/mainMenu/Thing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package mainMenu;
+
+/**
+ * @author Student 6
+ *
+ */
+public class Thing {
+
+	/**
+	 * 
+	 */
+	public Thing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
