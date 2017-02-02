569e9386bcc60b89fd39e5e0956cc792b59b7d6a
Student 6 <Student 6@10.8.33.100>
2017-02-02 12:20:05 -0500

    Created a Matthew file

---
+++ WorkoutPlanner/src/timer/MatthewThing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package timer;
+
+/**
+ * @author Student 6
+ *
+ */
+public class MatthewThing {
+
+	/**
+	 * 
+	 */
+	public MatthewThing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
