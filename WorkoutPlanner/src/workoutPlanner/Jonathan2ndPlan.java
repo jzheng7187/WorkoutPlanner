aeeb18851d232bc125f6f032c983510c15cfa5d8
Student 6 <Student 6@10.8.33.102>
2017-02-02 12:22:39 -0500

    made second file

---
+++ WorkoutPlanner/src/workoutPlanner/Jonathan2ndPlan.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package workoutPlanner;
+
+/**
+ * @author Jonathan Zheng
+ *
+ */
+public class Jonathan2ndPlan {
+
+	/**
+	 * 
+	 */
+	public Jonathan2ndPlan() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
