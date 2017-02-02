f23b543c0a02b55af68a65a559d5161a67821682
Student 6 <Student 6@BT_5E7_32>
2017-02-02 12:20:06 -0500

    created an Alam File

---
+++ WorkoutPlanner/src/timer/AlamsClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package timer;
+
+/**
+ * @author Alam
+ *
+ */
+public class AlamsClass {
+
+	/**
+	 * 
+	 */
+	public AlamsClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
