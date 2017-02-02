cd1c48c360c6fc40598b76be241e842d787d7adc
Student6 <Student6@10.8.33.176>
2017-02-02 12:22:25 -0500

    made a second file

---
+++ WorkoutPlanner/src/AnnaSecondThing.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Anna Ho
+ *
+ */
+public class AnnaSecondThing {
+
+	/**
+	 * 
+	 */
+	public AnnaSecondThing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
