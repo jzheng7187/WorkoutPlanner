f642bec5d5fdf4ac0a5a64da5c2101079718b63e
Student 6 <Student 6@10.8.33.100>
2017-02-08 12:30:09 -0500

    Update

--- WorkoutPlanner/src/workoutPlanner/NewExercise.java
+++ WorkoutPlanner/src/workoutPlanner/NewExercise.java
@@ -2,11 +2,13 @@ package workoutPlanner;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import gui.components.Action;
 import gui.components.Button;
 
-public class NewExercise extends Button {
+public class NewExercise extends Button implements MouseListener{
 	
 	private int exercise;
 	
@@ -16,5 +18,34 @@ public class NewExercise extends Button {
 	}
 	public void update(Graphics2D g){
 		
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override 
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isHovered(int x, int y) {
+		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
 	}
 }
