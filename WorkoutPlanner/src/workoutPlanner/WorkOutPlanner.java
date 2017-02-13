02e9ce2f22b04fe37af5d06d674abde9ae3cb916
Student6 <Student6@10.8.33.176>
2017-02-13 13:13:22 -0500

    2 13 17

--- WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
+++ WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
@@ -3,9 +3,11 @@ package workoutPlanner;
 import java.util.ArrayList;
 
 import gui.Screen;
+import gui.components.TextLabel;
 import gui.components.Visible;
 
 public class WorkOutPlanner extends Screen{
+	private TextLabel title;
 
 	public WorkOutPlanner(int width, int height) {
 		super(width, height);
@@ -15,7 +17,9 @@ public class WorkOutPlanner extends Screen{
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
 		// TODO Auto-generated method stub
-		
+		//title.setText("Plan Your Routine");
+		title = new TextLabel(130,230,300,40,"Plan Your Routine");
 	}
+	
 	
 }
