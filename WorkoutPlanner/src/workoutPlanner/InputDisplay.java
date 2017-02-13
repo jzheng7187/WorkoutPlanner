115488dee83500f3f25ac7ad42cb577503422267
Student6 <Student6@10.8.33.176>
2017-02-13 12:12:39 -0500

    anna 2 13 17

---
+++ WorkoutPlanner/src/workoutPlanner/InputDisplay.java
@@ -0,0 +1,21 @@
+package workoutPlanner;
+
+import java.util.ArrayList;
+
+import gui.Screen;
+import gui.components.Visible;
+
+public class InputDisplay extends Screen{
+
+public InputDisplay(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
