8c5967437f6898ac29ec4f0b2013a81538980793
Student6 <Student6@10.8.33.176>
2017-02-13 12:10:58 -0500

    workout planner 2 13 17

--- WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
+++ WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
@@ -1,5 +1,21 @@
 package workoutPlanner;
 
-public class WorkOutPlanner {
+import java.util.ArrayList;
 
+import gui.Screen;
+import gui.components.Visible;
+
+public class WorkOutPlanner extends Screen{
+
+	public WorkOutPlanner(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
