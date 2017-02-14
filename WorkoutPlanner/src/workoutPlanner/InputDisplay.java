7e73cbea0275ccb0b7fc45b575a0df4f795a1116
Student6 <Student6@10.8.33.176>
2017-02-14 12:33:41 -0500

    2 14 17

--- WorkoutPlanner/src/workoutPlanner/InputDisplay.java
+++ WorkoutPlanner/src/workoutPlanner/InputDisplay.java
@@ -3,9 +3,11 @@ package workoutPlanner;
 import java.util.ArrayList;
 
 import gui.Screen;
+import gui.components.TextLabel;
 import gui.components.Visible;
 
 public class InputDisplay extends Screen{
+	private TextLabel title;
 
 public InputDisplay(int width, int height) {
 		super(width, height);
@@ -15,7 +17,8 @@ public InputDisplay(int width, int height) {
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
 		// TODO Auto-generated method stub
-		
+		title = new TextLabel(130,230,300,40,"Plan Your Routine");
+		viewObjects.add(title);
 	}
 
 }
--- WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
+++ WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
@@ -3,22 +3,27 @@ package workoutPlanner;
 import java.util.ArrayList;
 
 import gui.Screen;
-import gui.components.TextLabel;
 import gui.components.Visible;
 
 public class WorkOutPlanner extends Screen{
-	private TextLabel title;
+	
 
 	public WorkOutPlanner(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void initScreen() {
+		InputDisplay ssa = new InputDisplay (getWidth(), getHeight());
+		setScreen(ssa);
+	}
+
 
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
 		// TODO Auto-generated method stub
 		//title.setText("Plan Your Routine");
-		title = new TextLabel(130,230,300,40,"Plan Your Routine");
+		
 	}
 	
 	
