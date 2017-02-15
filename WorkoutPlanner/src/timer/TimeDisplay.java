99072a522d06936cc585ab4898e746878e761d7c
Student6 <Student6@10.8.33.167>
2017-02-15 13:18:37 -0500

    needs work sadasdasdasd

---
+++ WorkoutPlanner/src/timer/TimeDisplay.java
@@ -0,0 +1,17 @@
+package timer;
+
+/**
+ * 
+ * @author Nikita Smirnov
+ *
+ */
+
+public interface TimeDisplay {
+	
+	public void markLongestTime(int i);
+	
+	public void markShortestTime(int i);
+	
+	public void markAverageTime(int i);
+	
+}
