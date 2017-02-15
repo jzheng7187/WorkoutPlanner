7c41f39628eb3fc20b39c32b724a7ea111cd20e6
Student 6 <Student 6@10.8.33.100>
2017-02-15 13:19:03 -0500

    Update

--- WorkoutPlanner/src/workoutPlanner/NewExercise.java
+++ WorkoutPlanner/src/workoutPlanner/NewExercise.java
@@ -11,31 +11,104 @@ import gui.components.Button;
 
 public class NewExercise extends Button implements MouseListener{
 	
-	//exercises ArrayList has all the exercises
-	private ArrayList<ButtonInterfaceMatthewN> exercises;
+	//allExercises ArrayList has all the exercises
+	private static ArrayList<String> allExercises;
+	//myExercises ArrayList has all the exercises you added
+	private static ArrayList<String> myExercises = new ArrayList<String>();
+	
 	//private ArrayList<ButtonInterfaceMatthewN> ;
-	private boolean clicked;
-	private int value;
-	private ButtonInterfaceMatthewN clickedGraphic;
+	private boolean[] clicked;
+	private int[] value;
+	private String clickedGraphic;
 	
 	//new exercise adds exercises into a list
 	public NewExercise(int x, int y, int w, int h, String text, Color color, Action action) {
 		super(x, y, w, h, text, color, action);
 		update();
 	}
-	public void update(Graphics2D g){
-		for(int i = 0; i < exercises.length(); i++){
-			value = 1;
-			clicked = false;
+	
+	public static void main(String[] args) {
+		getExercises();
+		for(int i = 0; i < allExercises.length(); i++){
+			value[i] = 1;
+			clicked[i] = false;
 		}
-		addExercise(exercises);
-		exercises.remove(clickedGraphic);
+		for(int i = 0; i < allExercises.length(); i++){
+			System.out.println(allExercises[i]);
+			System.out.println(value[i]);
+			System.out.println(clicked[i]);
+		}
+	}
+	
+	public void update(Graphics2D g){
+		value = new int[allExercises.length()];
+		clicked  = new boolean[allExercises.length()];
+		addExercise(allExercises);
+		allExercises.remove(clickedGraphic);
 	}
 	@Override
 	public void mouseClicked(MouseEvent g) {
-		return 
+		if(isHovered(getX(),getY())){
+			
+		}
 		
 	}
+	
+	@Override
+	public boolean isHovered(int x, int y) {
+		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
+	}
+	
+	//highlights picture when mouse hovers over image
+	public boolean highlight(){
+		for(int i = 0; i <  allExercises.length(); i++){
+			if(isHovered(getX(),getY())){
+				return allExercises[i].setColor(new Color(255, 255, 255, 25));
+			}else{
+				return false;
+			}
+		}
+	}
+	
+	//checks if exercise that is clicked on was already clicked on or not.  
+	//If it was not yet clicked on it will set the value to 0
+	public boolean isClicked(){
+		for(int i = 0; i < value.length; i++){
+			if(value[i] == 1){
+				value[i]--;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//adds exercises to myExercises
+	public void addExercise(ArrayList<String> exercises){
+		boolean clickedAgain = false;
+		
+		for(int i = 0; i < value.length; i++){
+			if(value[i] == 0 && isHovered()){
+				System.out.println("You have already chosen this exercise.");
+				exercises.highlight();
+			}else{
+				myExercises.add(allExercises[i]);
+				allExercises.remove(allExercises[i]);
+			}
+		}
+		update();
+	}
+	
+	public static void getExercises(){
+		allExercises = new ArrayList<String>();
+		allExercises.add("Push-up");
+		allExercises.add("Curl-up");
+		allExercises.add("Sit-up");
+		allExercises.add("Bench Press");
+		allExercises.add("Plank");
+		allExercises.add("Dips");
+		allExercises.add("Squats");
+	}
+	
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
@@ -55,33 +128,5 @@ public class NewExercise extends Button implements MouseListener{
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
-	}
-	@Override
-	public boolean isHovered(int x, int y) {
-		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
-	}
-	
-	//highlights picture when mouse hovers over image
-	public boolean highlight(){
-		for(int i = 0; i <  exercises.length(); i++)
-		if(isHovered(getX(),getY())){
-			return exercises[i].setColor(new Color(255, 255, 255, 25));
-		}else{
-			return false;
-		}
-	}
-	public boolean isClicked(){
-		return true;
-	}
-	
-	
-	public void addExercise(ArrayList<ButtonInterfaceMatthewN> g){
-		boolean clickedAgain = false;
-		if(isClicked()){
-			System.out.println("You have already chosen this exercise.");
-			g.highlight();
-		}else{
-			exercises.add(clickedGraphic);
-		}
 	}
 }
