da5960f2e4dd28ffcad0c6c6990ac71a7c1c2903
thesheild <aho6595@bths.edu>
2017-02-15 13:03:10 -0500

    Update WorkOutPlanner.java

--- WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
+++ WorkoutPlanner/src/workoutPlanner/WorkOutPlanner.java
@@ -1,42 +1,24 @@
 package workoutPlanner;
 
-import java.util.ArrayList;
 //import gui.Screen;
-import gui.components.Visible;
-import gui.screens.ClickableScreen;
-import gui.GuiApplication;
+import gui.GUIApplication;
 
-public class WorkOutPlanner extends ClickableScreen{
+public class WorkOutPlanner extends GUIApplication{
 	
-
 	public WorkOutPlanner(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void initScreen() {
-		InputDisplay ssa = new InputDisplay (getWidth(), getHeight());
-		setScreen(ssa);
+		InputDisplay wo = new InputDisplay (getWidth(), getHeight());
+		setScreen(wo);
 	}
 
-
-	@Override
-	public void initObjects(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		//title.setText("Plan Your Routine");
-		
-	}
 	
 	public static void main(String[] args) {
-		InputDisplay sga = new InputDisplay(800,500);
-		Thread game = new Thread(sga);
-		game.start();
-	}
-
-	@Override
-	public void initAllObjects(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		
+		InputDisplay ID = new InputDisplay(800,500);
+		Thread planner = new Thread(ID);
+		planner.start();
 	}
 
 	
