ae3b2190fca2fe1166cfff1204f34b1b21440b1a
Teacher <Teacher@10.148.46.60>
2017-03-01 14:50:23 -0500

    3 1 17 thanks mr nockles

--- WorkoutPlanner/src/workoutPlanner/InputDisplay.java
+++ WorkoutPlanner/src/workoutPlanner/InputDisplay.java
@@ -4,15 +4,18 @@ import java.awt.Color;
 import java.util.ArrayList;
 //import components.ThemedTextLabel;
 
+import components.ThemedTextLabel;
+import gui.components.Graphic;
 //import gui.Screen;
 import gui.components.TextLabel;
 import gui.components.Visible;
 import gui.screens.ClickableScreen;
 
 public class InputDisplay extends ClickableScreen implements Runnable{
+	private ThemedTextLabel accentTitle;
 	private TextLabel title;
 	private TextLabel sub;
-	//public static final Color c = new Color (180,225,50);
+	private Graphic ex;
 
 public InputDisplay(int width, int height) {
 		super(width, height);
@@ -29,10 +32,15 @@ public InputDisplay(int width, int height) {
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
 		// TODO Auto-generated method stub
 		//ThemedTextLabel(50,10,50,30, "Plan", 10);
-		title = new TextLabel(100,100,300,40,"Plan Your Routine");
-		viewObjects.add(title); 
+		
+		accentTitle = new ThemedTextLabel(140,100,300,40,"Your Routine", new Color(180,225,50));
+		title = new TextLabel(100,100,300,40,"Plan");
+		viewObjects.add(title);
+		viewObjects.add(accentTitle); 
 		sub = new TextLabel(300,300,400,60,"Activities");
 		viewObjects.add(sub);
+		ex = new Graphic(300, 200, .5, "resources/Capture.PNG");
+		viewObjects.add(ex);
 	}
 
 }
