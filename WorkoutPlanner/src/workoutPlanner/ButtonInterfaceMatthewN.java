16d99fae71c1b2895e5ef3a073915a2d3ede2af7
matth <matth@Matthew-Predator-PC.home>
2017-03-02 21:45:28 -0500

    Commit and Push From Home Matt

--- WorkoutPlanner/src/workoutPlanner/ButtonInterfaceMatthewN.java
+++ WorkoutPlanner/src/workoutPlanner/ButtonInterfaceMatthewN.java
@@ -3,14 +3,13 @@ package workoutPlanner;
 import gui.components.Clickable;
 
 public interface ButtonInterfaceMatthewN extends Clickable {
-	
 	public void showExercises();
 	public void highlight(); 
 	
-	public void setTreadmill();
-	public void setSquats();
-	public void setCrunches();
-	public void setCycling();
-	public void setPushup();
-	public void setJumprope();
+	public void clickTreadmill();
+	public void clickSquats();
+	public void clickCrunches();
+	public void clickCycling();
+	public void clickPushup();
+	public void clickJumprope();
 }
